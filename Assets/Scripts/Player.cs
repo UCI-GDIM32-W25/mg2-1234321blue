d9dc70cf2f32febf7b2fa7d19671594666130f92
1234321blue <djeremiahyang@gmail.com>
2026-01-13 19:45:29 -0800

    jumping done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,7 +23,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
