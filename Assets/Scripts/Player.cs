9b439635a8c09dd64bf7cce390028245b5f5e71d
1234321blue <djeremiahyang@gmail.com>
2026-01-13 19:37:14 -0800

    yes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _playerRB;
+    [SerializeField] float _jump;
+    bool _isGrounded=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&&_isGrounded)
+        {
+            _playerRB.AddForce(Vector2.up*_jump,ForceMode2D.Impulse);
+            _isGrounded=false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded=true;
+        }
+    }
+}
