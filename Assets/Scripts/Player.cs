fda11c1d76e82a580353e709651d8c1f3895b5c3
1234321blue <djeremiahyang@gmail.com>
2026-01-13 19:40:58 -0800

    yes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("testing");
     }
 
     // Update is called once per frame
